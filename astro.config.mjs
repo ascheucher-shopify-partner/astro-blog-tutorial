791792327cc77c9519f82977d32bca979413fc92
Andreas Scheucher <andreas.scheucher@gmail.com>
2024-05-23 18:56:34 +0200

    test first base URL config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ascheucher-shopify-partner.github.io',
-  base: '/astro-blog-tutorial'
+  site: process.env.NODE_ENV === 'development' ? 'http://localhost:4321' : 'https://ascheucher-shopify-partner.github.io',
+  base: '/astro-blog-tutorial',
 });
