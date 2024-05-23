8e482eac8e0be7d065e337ca607121575b790189
Andreas Scheucher <andreas.scheucher@gmail.com>
2024-05-23 20:14:44 +0200

    navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: process.env.NODE_ENV === 'development' ? 'http://localhost:4321' : 'https://ascheucher-shopify-partner.github.io',
   base: 'astro-blog-tutorial',
-  trailingSlash: "never"
+  trailingSlash: "always"
 });
