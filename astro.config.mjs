d0183c05f456ad90bca5b5f3f2c948f3e62f7760
Andreas Scheucher <andreas.scheucher@gmail.com>
2024-05-23 19:40:42 +0200

    fix navigation paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: process.env.NODE_ENV === 'development' ? 'http://localhost:4321' : 'https://ascheucher-shopify-partner.github.io',
-  base: '/astro-blog-tutorial',
+  base: 'astro-blog-tutorial',
+  trailingSlash: "never"
 });
