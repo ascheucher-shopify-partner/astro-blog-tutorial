fcdfc70a9d0d41367611dcf86ea3543c68a40b64
Andreas Scheucher <andreas.scheucher@gmail.com>
2024-05-24 14:36:00 +0200

    First preact component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.NODE_ENV === 'development' ? 'http://localhost:4321' : 'https://ascheucher-shopify-partner.github.io',
   base: 'astro-blog-tutorial',
-  trailingSlash: "always"
+  trailingSlash: "always",
+  integrations: [preact()]
 });
