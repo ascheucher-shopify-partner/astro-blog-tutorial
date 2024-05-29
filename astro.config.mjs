83fdeb9cf193a35887c9e086275c8cc5246cbe83
Andreas Scheucher <andreas.scheucher@gmail.com>
2024-05-29 15:46:46 +0200

    root redirect to the default language, added i18n TODOs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: process.env.NODE_ENV === 'development' ? 'http://localhost:4321' : 'https://ascheucher-shopify-partner.github.io',
   site: 'https://ascheucher-shopify-partner.github.io',
   base: '/astro-blog-tutorial',
   trailingSlash: "never",
